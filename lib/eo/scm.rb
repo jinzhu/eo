7b94ee51119398e7767f551e83676d44c8135ac8
wosmvp <wosmvp@gmail.com>
2008-12-12 20:49:58 +0800

    Add SVN support

--- lib/eo/scm.rb
+++ lib/eo/scm.rb
@@ -4,14 +4,38 @@ class Scm < Hash
     Dir.chdir(File.expand_path(self['path']))
     puts "\e[31m" + self['path'] + "\e[0m"
 
-    old_commit = `git log --pretty=format:%H -1` #TODO SVN
-    system("git pull")
-    new_commit = `git log --pretty=format:%H -1`
+    old_commit = now_commit
+    scm_update
+    new_commit = now_commit
     if new_commit == old_commit
       puts "NO Update"
     else
       eval self['autorun'] if self['autorun']
     end
+  end
+
+  def scm
+    Dir.chdir(File.expand_path(self['path']))
+    return 'git' if !Dir.entries('.').grep(".git").empty?
+    return 'svn' if !Dir.entries('.').grep(".svn").empty?
+  end
+
+  def scm_update
+    Dir.chdir(File.expand_path(self['path']))
+    case scm
+    when /git/ if system("git pull")
+    when /svn/ if system("svn update")
+    end
+  end
+
+  def now_commit
+   case scm
+   when /git/ then return `git log --pretty=format:%H -1`
+   when /svn/ then return `svn -l 1 log -q | grep "\w" | awk '{print $1}'`
+   else
+     puts "\e[31mSorry,RightNow Only Support SVN/GIT\e[0m"
+     return false
+   end
   end
 
   def shell
