a1d0ada68a0dd74641452948cd02b688cc800c88
wosmvp <wosmvp@gmail.com>
2008-12-12 21:04:27 +0800

    Fix Typo

--- lib/eo/scm.rb
+++ lib/eo/scm.rb
@@ -14,35 +14,10 @@ class Scm < Hash
     end
   end
 
-  def scm
-    Dir.chdir(File.expand_path(self['path']))
-    return 'git' if !Dir.entries('.').grep(".git").empty?
-    return 'svn' if !Dir.entries('.').grep(".svn").empty?
-  end
-
-  def scm_update
-    Dir.chdir(File.expand_path(self['path']))
-    case scm
-    when /git/ if system("git pull")
-    when /svn/ if system("svn update")
-    end
-  end
-
-  def now_commit
-   case scm
-   when /git/ then return `git log --pretty=format:%H -1`
-   when /svn/ then return `svn -l 1 log -q | grep "\w" | awk '{print $1}'`
-   else
-     puts "\e[31mSorry,RightNow Only Support SVN/GIT\e[0m"
-     return false
-   end
-  end
-
   def shell
     Dir.chdir(File.expand_path(self['path']))
     system("sh")
   end
-
   alias sh shell
 
   def method_missing(m,*args)
@@ -56,5 +31,28 @@ class Scm < Hash
       result = system(m + " " + args.join(' '))
       puts "Some Wrong?" unless result
     end
+  end
+
+  protected
+  def scm
+    return 'git' if !Dir.entries('.').grep(".git").empty?
+    return 'svn' if !Dir.entries('.').grep(".svn").empty?
+  end
+
+  def scm_update
+    case scm
+    when /git/ then system("git pull")
+    when /svn/ then system("svn update")
+    end
+  end
+
+  def now_commit
+   case scm
+   when /git/ then return `git log --pretty=format:%H -1`
+   when /svn/ then return `svn -l 1 log -q | grep "\w" | awk '{print $1}'`
+   else
+     puts "\e[31mSorry,RightNow Only Support SVN/GIT\e[0m"
+     return false
+   end
   end
 end
