0c396c0266227b46c312ffeb58f8d038fdb0fefb
wosmvp <wosmvp@gmail.com>
2009-01-03 16:10:13 +0800

    tidy help document

--- lib/eo.rb
+++ lib/eo.rb
@@ -17,32 +17,18 @@ class Eo
   class << self
     def execute(args)
       case args.first
-      when "-gs" then self.gemshow(args[1,args.size])
-      when "-gc" then self.gemshell(args[1,args.size])
-      when "-go" then self.gemopen(args[1,args.size])
-      when "-s"  then self.show(args[1,args.size])
-      when "-o"  then self.open(args[1,args.size])
-      when "-c"  then self.choose(args[1,args.size])
-      when "-i"  then self.init(args[1,args.size])
-      when "-u"  then self.update(args[1,args.size])
-      when "-t"  then self.type
-      when "-v"  then puts "\e[33mEo_oE : v" + Easyoperate::VERSION + "\e[0m"
-      when "-h"  then
-        puts <<-DOC.gsub(/^(\s*\|)/,'')
-        |Usage: #{File.basename($0)} [options] [ARGS]
-
-        |Options are:
-        |  -i        Initialize Repository.     <Regexp>
-        |  -t        Show All Support Scm
-        |  -u        Update Repository.         <Regexp>
-        |  -s        Show All Repositories.     <Regexp>
-        |  -o        Open The repository's path <Regexp>
-        |  -c        Choose Repository.         <Regexp>
-        |  -gs       Show All Gems.             <Regexp>
-        |  -gc       Choose Gem.                <Regexp>
-        |  -go       Open The Gem's Path        <Regexp>
-        |  -v        Show version information.
-        DOC
+      when /^-gs$/i then gemshow(args[1,args.size])
+      when /^-gc$/i then gemshell(args[1,args.size])
+      when /^-go$/i then gemopen(args[1,args.size])
+      when /^-s$/i  then show(args[1,args.size])
+      when /^-o$/i  then open(args[1,args.size])
+      when /^-c$/i  then choose(args[1,args.size])
+      when /^-i$/i  then init(args[1,args.size])
+      when /^-u$/i  then update(args[1,args.size])
+      when /^-t$/i  then type
+      when /^-v$/i  then
+            puts "\e[33mEo_oE : v" + Easyoperate::VERSION + "\e[0m"
+      when /^-(h|help)$/i  then help
       else self.run
       end
     end
@@ -54,24 +40,28 @@ class Eo
         input = (STDIN.gets || exit).rstrip.split(' ',2)
 
         case input[0].to_s
-        when /GS/i then gemshow(input[1])
-        when /GC/i then gemshell(input[1])
-        when /GO/i then gemopen(input[1])
-        when /S/i  then show(input[1])
-        when /O/i  then open(input[1])
-        when /C/i  then choose(input[1])
-        when /U/i  then update(input[1])
-        when /I/i  then init(input[1])
-        when /T/i  then type
-        when /Q/i  then exit
-        else help
+        when /^GS$/i then gemshow(input[1])
+        when /^GC$/i then gemshell(input[1])
+        when /^GO$/i then gemopen(input[1])
+        when /^S$/i  then show(input[1])
+        when /^O$/i  then open(input[1])
+        when /^C$/i  then choose(input[1])
+        when /^U$/i  then update(input[1])
+        when /^I$/i  then init(input[1])
+        when /^T$/i  then type
+        when /^Q$/i  then exit
+        when /^V$/i  then
+          puts "\e[33mEo_oE : v" + Easyoperate::VERSION + "\e[0m"
+        when /^H|HELP$/i  then help
+        else puts "\e[31mError Command\e[0m"
         end
       end
     end
 
-    def help
-      puts <<-DOC.gsub(/^(\s*\|)/,'')
-      |Usage:
+    def help(shell=true)
+      puts <<-DOC.gsub(/^(\s*)/,'').gsub(/^\|(\s*)/,'\1' + (shell ? '-':''))
+      Usage:
+
       |  I /args/ : Initialize matched Repository <Regexp>
       |  U /args/ : Update matched Repository     <Regexp>
       |  T        : Show All Support Scm
@@ -83,7 +73,9 @@ class Eo
       | GO /args/ : Open The Gem's Path           <Regexp>
       |  Q        : Quit
       |  H        : Show this help message.
-      |e.g:\n  \e[32m s v.*m\e[0m
+      |  V        : Show version information.
+      #{shell ? "e.g: \n \e[032m $ eo -S v.*m\e[0m" :
+                "e.g:\n  \e[32m s v.*m\e[0m" }
       DOC
     end
   end
